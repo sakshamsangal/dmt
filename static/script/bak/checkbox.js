880500e5f0683570e7b6be942d8c1439c5939502
Saksham Sangal <sakshamsangal111@gmail.com>
2022-04-12 14:07:41 +0530

    remove js

--- static/script/bak/checkbox.js
+++
@@ -1,7 +0,0 @@
-$('#has_rule').change(function () {
-    tag_dict_from_backend[selected_tag_name]['has_rule'] = !tag_dict_from_backend[selected_tag_name]['has_rule']
-});
-
-$('#has_query').change(function () {
-    tag_dict_from_backend[selected_tag_name]['has_query'] = !tag_dict_from_backend[selected_tag_name]['has_query']
-});
