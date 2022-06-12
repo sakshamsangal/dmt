4c82220a2284509d017953d875e9762311aac4f5
Saksham Sangal <sakshamsangal111@gmail.com>
2022-06-13 01:33:11 +0530

    lc

--- resume.py
+++ resume.py
@@ -1,18 +1,28 @@
 import json
 
 from docx.shared import Cm
-from docxtpl import DocxTemplate, InlineImage
-
+from docxtpl import DocxTemplate, InlineImage, RichText
 
 
 def gen_docx():
     doc = DocxTemplate("static/doc/in/res.docx")
-    img_size = Cm(10)  # sets the size of the image
+
+
+    img_size = Cm(0.7)  # sets the size of the image
     with open('resume.json') as f:
         x = json.load(f)
-    doc.render(x)
-    doc.save('static/doc/out/res.docx')
+    y = x['work_exp']['comp']
+    for item in y:
+        item['img'] = InlineImage(doc, item['img'], img_size)
 
+    for item in x['ref']:
+        rt = RichText()
+        rt.add(item['link'], url_id=doc.build_url_id(item['link']))
+        item['rt'] = rt
+
+    print(x['ref'])
+    doc.render(x)
+    doc.save('static/doc/out/res_out.docx')
 
 
 gen_docx()
